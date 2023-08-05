b6a5ee819b0a60a97171a8251cd8ba6ca15c87f8
Hyporos <59734777+Hyporos@users.noreply.github.com>
2023-08-05 02:18:23 -0400

    Skills Logos

--- app/components/Conclusion/Conclusion.tsx
+++ app/components/Conclusion/Conclusion.tsx
@@ -1,11 +1,25 @@
-import './Conclusion.css'
+import "./Conclusion.css";
 
 const Conclusion = () => {
   return (
     <section>
-    <div>Conclusion</div>
-  </section>
-  )
-}
+      <div className="flex tracking-wide flex-col space-y-4 bg-secondary/75 justify-center items-center rounded-3xl w-[900px] h-[400px]">
+        <p className="text-text text-center text-xl font-light">
+          I&apos;m a web developer with a passion for design based in Toronto,
+          Canada.
+        </p>
+        <p className="text-text text-center text-xl font-light">
+          I strive to create aesthetic applications that prove to be useful to
+          others.
+        </p>
+        <p className="text-text text-center text-xl font-light">
+          When I&apos;m not occupied with coding, you can find me hiking in
+          nature,
+          <br></br> studying languages, or spending time online.
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default Conclusion
+export default Conclusion;
--- app/components/Skills/Skills.tsx
+++ app/components/Skills/Skills.tsx
@@ -1,11 +1,128 @@
-import './Skills.css'
+import "./Skills.css";
+import Image from "next/image";
 
 const Skills = () => {
   return (
     <section>
-    <div>Skills</div>
-  </section>
-  )
-}
+      <div className="flex tracking-wide flex-col space-y-4 bg-secondary/75 justify-center items-center rounded-3xl w-[900px] h-[400px]">
+        <div>
+          <h2 className="text-text text-center text-xl font-light">
+            Front-End
+          </h2>
+          <div className="flex space-x-8">
+            <Image
+              src="/badges/HTML5.webp"
+              alt="HTML5"
+              draggable="false"
+              width={75}
+              height={75}
+            ></Image>
+            <Image
+              src="/badges/CSS3.webp"
+              alt="CSS3"
+              draggable="false"
+              width={75}
+              height={75}
+            ></Image>
+            <Image
+              src="/badges/JS.webp"
+              alt="JavaScript"
+              draggable="false"
+              width={75}
+              height={75}
+            ></Image>
+            <Image
+              src="/badges/TS.webp"
+              alt="TypeScript"
+              draggable="false"
+              width={75}
+              height={75}
+            ></Image>
+            <Image
+              src="/badges/React.webp"
+              alt="React & Raact Native"
+              draggable="false"
+              width={75}
+              height={75}
+            ></Image>
+            <Image
+              src="/badges/Next.webp"
+              alt="Next.js"
+              draggable="false"
+              width={75}
+              height={75}
+            ></Image>
+            <Image
+              src="/badges/Tailwind.webp"
+              alt="Tailwind CSS"
+              draggable="false"
+              width={75}
+              height={75}
+            ></Image>
+          </div>
+        </div>
+        <div>
+          <h2 className="text-text text-center text-xl font-light">Back-End</h2>
+          <div className="flex space-x-8">
+            <Image
+              src="/badges/C++.webp"
+              alt="C++"
+              draggable="false"
+              width={75}
+              height={75}
+            ></Image>
+                        <Image
+              src="/badges/SQL.webp"
+              alt="SQL"
+              draggable="false"
+              width={75}
+              height={75}
+            ></Image>
+                                    <Image
+              src="/badges/Express.webp"
+              alt="Express.js"
+              draggable="false"
+              width={75}
+              height={75}
+            ></Image>
+          </div>
+        </div>
+        <div>
+          <h2 className="text-text text-center text-xl font-light">Other</h2>
+          <div className="flex space-x-8">
+            <Image
+              src="/badges/NodeJS.webp"
+              alt="Node.js"
+              draggable="false"
+              width={75}
+              height={75}
+            ></Image>
+            <Image
+              src="/badges/MongoDB.webp"
+              alt="MongoDB"
+              draggable="false"
+              width={75}
+              height={75}
+            ></Image>
+            <Image
+              src="/badges/Git.webp"
+              alt="Git"
+              draggable="false"
+              width={75}
+              height={75}
+            ></Image>
+            <Image
+              src="/badges/Figma.webp"
+              alt="Figma"
+              draggable="false"
+              width={75}
+              height={75}
+            ></Image>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Skills
+export default Skills;

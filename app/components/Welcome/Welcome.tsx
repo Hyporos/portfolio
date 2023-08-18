582da2d17efed33097b99f77b2b9512a1906646f
Hyporos <59734777+Hyporos@users.noreply.github.com>
2023-08-18 13:47:56 -0400

    Responsive Welcome

--- app/components/Welcome/Welcome.tsx
+++ app/components/Welcome/Welcome.tsx
@@ -6,17 +6,18 @@ const Welcome = () => {
   return (
     <section className="welcome">
       <ParticleBackground />
-      <div className="flex flex-col text-center space-y-6 text-text font-extralight tracking-wide select-none">
-        <h1 className="text-7xl">
+      <div className="flex p-6 desktop:p-0 flex-col text-center space-y-6 text-text font-extralight tracking-wide select-none">
+        <h1 className="text-5xl desktop:text-7xl">
           Hey! I&apos;m <span className="text-accent font-light">Brian
           Maleki</span>.
         </h1>
-        <h2 className="text-6xl">
+        <h2 className="text-4xl desktop:text-6xl">
           Welcome to my portfolio.
         </h2>
       </div>
-      <div className="absolute align-bottom mt-[800px] animate-[bounce_2s_ease-in-out_infinite] opacity-90 cursor-pointer">
-      <AiOutlineArrowDown size={75} fill={"#d586ff"}/>
+      <div className="absolute align-bottom mt-[500px] desktop:mt-[800px] animate-[bounce_2s_ease-in-out_infinite] opacity-90 cursor-pointer">
+      <AiOutlineArrowDown size={75} fill={"#d586ff"} className="hidden desktop:block"/>
+      <AiOutlineArrowDown size={60} fill={"#d586ff"} className="block desktop:hidden"/>
       </div>
     </section>
   );

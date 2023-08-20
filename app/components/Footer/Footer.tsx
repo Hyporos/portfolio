fcbcde37210df4a12e6364c579a9839637f51be1
Hyporos <59734777+Hyporos@users.noreply.github.com>
2023-08-20 17:03:44 -0400

    Footer Cleanup

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,40 +1,40 @@
 "use client";
-import Image from "next/image";
-import "./Footer.css";
-import { useMediaQuery } from 'react-responsive';
-
+import { useMediaQuery } from "react-responsive";
 import { BiLogoReact, BiLogoTailwindCss } from "react-icons/bi";
 import { TbBrandNextjs, TbBrandVercel } from "react-icons/tb";
+import "./Footer.css";
 
 const Footer = () => {
-  const isMobile = useMediaQuery({ query: '(max-width: 1099px)' })
+  const isMobile = useMediaQuery({ query: "(max-width: 1099px)" });
   return (
-    <section className="footer bg-background/50 h-[170px] desktop:h-[200px]">
+    <section className="footer">
       <div className="flex flex-col items-center space-y-4 desktop:space-y-6">
-        <p className="text-text text-lg desktop:text-xl text-center">Built with</p>
+        <p className="text-text text-center text-lg desktop:text-xl">
+          Built with
+        </p>
         <div className="flex space-x-3 desktop:space-x-4">
           <a href="https://react.dev/" target="_blank">
             <BiLogoReact
               size={isMobile ? 35 : 40}
-              className="fill-primary hover:fill-accent transition-all duration-300 opacity-85"
+              className="logo-icon fill-primary hover:fill-accent"
             />
           </a>
           <a href="https://nextjs.org/" target="_blank">
             <TbBrandNextjs
               size={isMobile ? 35 : 40}
-              className="stroke-primary hover:stroke-accent transition-all duration-300 opacity-85"
+              className="logo-icon stroke-primary hover:stroke-accent"
             />
           </a>
           <a href="https://vercel.com/" target="_blank">
             <TbBrandVercel
               size={isMobile ? 35 : 40}
-              className="stroke-primary hover:stroke-accent transition-all duration-300 opacity-85"
+              className="logo-icon stroke-primary hover:stroke-accent"
             />
           </a>
           <a href="https://tailwindcss.com/" target="_blank">
             <BiLogoTailwindCss
               size={isMobile ? 35 : 40}
-              className="fill-primary hover:fill-accent transition-all duration-300 opacity-85"
+              className="logo-icon fill-primary hover:fill-accent"
             />
           </a>
         </div>

6d414ce53469ca39aa1bd3c7da5b3601db1ce2b8
Brian Maleki <hyporos@gmail.com>
2023-08-21 19:08:54 -0400

    Responsive Welcome (tablet)

--- app/components/Welcome/Welcome.tsx
+++ app/components/Welcome/Welcome.tsx
@@ -5,21 +5,21 @@ import { AiOutlineArrowDown } from 'react-icons/ai';
 import "./Welcome.css";
 
 const Welcome = () => {
-  const isMobileOrTablet = useMediaQuery({ query: "(max-width: 1099px)" });
+  const isMobile = useMediaQuery({ query: "(max-width: 749px)" });
   return (
     <section className="welcome">
       <ParticleBackground />
       <div className="flex flex-col text-text font-extralight text-center tracking-wide select-none space-y-6 p-6 desktop:p-0">
-        <h1 className="text-5xl desktop:text-7xl">
+        <h1 className="text-5xl tablet:text-6xl desktop:text-7xl">
           Hey! I&apos;m <span className="text-accent font-light">Brian
           Maleki</span>.
         </h1>
-        <h2 className="text-4xl desktop:text-6xl">
+        <h2 className="text-4xl tablet:text-5xl desktop:text-6xl">
           Welcome to my portfolio.
         </h2>
       </div>
       <div className="absolute opacity-90 cursor-pointer animate-[bounce_2s_ease-in-out_infinite] align-bottom mt-[500px] desktop:mt-[800px]">
-      <AiOutlineArrowDown size={!isMobileOrTablet ? 75 : 60} fill={"#d586ff"}/>
+      <AiOutlineArrowDown size={!isMobile ? 75 : 60} fill={"#d586ff"}/>
       </div>
     </section>
   );

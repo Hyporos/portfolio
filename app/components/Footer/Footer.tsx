a351431cc6f2458f2f5c5f62bee1a6821132c96e
Brian Maleki <hyporos@gmail.com>
2023-08-23 02:37:41 -0400

    Intersection Observer (Footer)

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,18 +1,32 @@
 "use client";
+import { useRef } from "react";
 import { useMediaQuery } from "react-responsive";
 import { BiLogoReact, BiLogoTailwindCss } from "react-icons/bi";
 import { TbBrandNextjs, TbBrandVercel } from "react-icons/tb";
+import useOnScreen from "@/app/hooks/isOnScreen";
 import "./Footer.css";
 
 const Footer = () => {
   const isMobile = useMediaQuery({ query: "(max-width: 749px)" });
+  const observerRef = useRef<HTMLDivElement>(null);
+  const isOnScreen = useOnScreen(observerRef);
   return (
-    <section className="footer">
+    <section ref={observerRef} className="footer">
       <div className="flex flex-col items-center space-y-4 desktop:space-y-6">
-        <p className="text-text text-center text-lg desktop:text-xl">
+        <p
+          className={`text-text text-center text-lg desktop:text-xl transition-all duration-[2500ms] ${
+            isOnScreen
+              ? "translate-y-[0px] opacity-100"
+              : "translate-y-[-20px] opacity-0"
+          }`}
+        >
           Built with
         </p>
-        <div className="flex space-x-3 desktop:space-x-4">
+        <div
+          className={`flex space-x-3 desktop:space-x-4 transition-all duration-[2500ms] ${
+            !isOnScreen && "opacity-0"
+          }`}
+        >
           <a href="https://react.dev/" target="_blank">
             <BiLogoReact
               size={isMobile ? 35 : 40}
@@ -38,7 +52,10 @@ const Footer = () => {
             />
           </a>
         </div>
-        <p className="text-text/40 text-center">&copy; 2023 Brian Maleki</p>
+        <p className={`text-text/40 text-center transition-all duration-[2500ms] ${isOnScreen
+              ? "translate-y-[0px] opacity-100"
+              : "translate-y-[20px] opacity-0"
+          }}`}>&copy; 2023 Brian Maleki</p>
       </div>
     </section>
   );
